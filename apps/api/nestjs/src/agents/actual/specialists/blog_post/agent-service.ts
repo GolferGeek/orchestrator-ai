199be0af0e8300b26fdf30e7750e3d1350a6a456
golfergeek <golfergeet@gmail.com>
2025-06-18 11:48:51 -0400

    context base class working with refactor

--- apps/api/nestjs/src/agents/actual/specialists/blog_post/agent-service.ts
+++ apps/api/nestjs/src/agents/actual/specialists/blog_post/agent-service.ts
@@ -1,10 +1,14 @@
 import { Injectable } from '@nestjs/common';
 import { HttpService } from '@nestjs/axios';
+import { LLMService } from '@/llms/llm.service';
 import { ContextAgentBaseService } from '@agents/base/implementations/base-services/context/context-agent-base.service';
 
 @Injectable()
 export class BlogPostService extends ContextAgentBaseService {
-  constructor(httpService: HttpService) {
-    super(httpService);
+  constructor(
+    httpService: HttpService,
+    llmService: LLMService
+  ) {
+    super(httpService, llmService);
   }
 } 
